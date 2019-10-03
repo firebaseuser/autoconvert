394f28f8a2603ef827065c1702761f9d13f4259b
autoconvert <firebaseuser678@gmail.com>
2019-10-03 10:54:01 +0630

    Active Library1

--- app/src/main/java/com/example/autoconverter/MyApplication.java
+++ app/src/main/java/com/example/autoconverter/MyApplication.java
@@ -2,8 +2,6 @@ package com.example.autoconverter;
 
 import android.app.Application;
 
-import me.myatminsoe.mdetect.MDetect;
-
 public class MyApplication extends Application {
     @Override
     public void onCreate() {
