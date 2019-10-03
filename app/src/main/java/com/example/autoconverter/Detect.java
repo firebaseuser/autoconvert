ad827871c10e2c32748499ba91c3663a8925d586
autoconvert <firebaseuser678@gmail.com>
2019-10-03 10:19:46 +0630

    Active Library

---
+++ app/src/main/java/com/example/autoconverter/Detect.java
@@ -0,0 +1,24 @@
+package com.example.autoconverter;
+
+import me.myatminsoe.mdetect.MDetect;
+import me.myatminsoe.mdetect.Rabbit;
+
+public class Detect {
+    public static String convert(String to, String text){
+        String returnText = text;
+        if (to.equals("u")) {
+            if (MDetect.INSTANCE.isUnicode()) {
+                returnText=text;
+            }else {
+                returnText= Rabbit.zg2uni(text);
+            }
+        }else if (to.equals("z")){
+            if (MDetect.INSTANCE.isUnicode()) {
+                returnText=Rabbit.uni2zg(text);
+            }else {
+                returnText= text;
+            }
+        }
+        return returnText;
+    }
+}
---
+++ app/src/main/java/com/example/autoconverter/MyApplication.java
@@ -0,0 +1,13 @@
+package com.example.autoconverter;
+
+import android.app.Application;
+
+import me.myatminsoe.mdetect.MDetect;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        MDetect.INSTANCE.init(this);
+    }
+}
